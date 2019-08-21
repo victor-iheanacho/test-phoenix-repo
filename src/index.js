fdc60024eda5599e5fff23dcd73ca7f5f3ca7319
temisan otokuefor <temipretty1992@gmail.com>
2019-08-21 01:34:52 +0100

    chore(refactors code) - Refactors the code base to es6
    - Resolve commit errors
    - Resolve conflicts
    - Refactor codebase to es6
    - fix hound errors
    - moved the public folder outside the src folder
    - ensures server is running
    
    [Finishes #167982558]

--- src/index.js
+++ src/index.js
@@ -1,94 +1,37 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+import errorhandler from 'errorhandler';
 import { serve, setup } from 'swagger-ui-express';
-import swaggerDoc from './public/api-docs/swaggerDoc';
-
-const fs = require('fs'),
-  http = require('http'),
-  path = require('path'),
-  methods = require('methods'),
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  session = require('express-session'),
-  cors = require('cors'),
-  passport = require('passport'),
-  errorhandler = require('errorhandler');
+import swaggerDoc from '../public/api-docs/swaggerDoc';
+import Router from './routes';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 // Create global app object
 const app = express();
+app.use(cors());
 
 // generate api documentation
 app.use('/api-docs', serve, setup(swaggerDoc));
 
-app.use(cors());
-
 // Normal express config defaults
-app.use(require('morgan')('dev'));
+app.use(morgan('dev'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.use(require('method-override')());
-
-app.use(express.static(`${__dirname}/public`));
-
-app.use(
-  session({
-    secret: 'authorshaven',
-    cookie: { maxAge: 60000 },
-    resave: false,
-    saveUninitialized: false
-  })
-);
 
 if (!isProduction) {
   app.use(errorhandler());
 }
 
+app.get('/', (_req, res) => res.status(301).redirect('/api'));
+app.use('/api', Router);
 
-
-// / catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// / error handlers
-
-// development error handler
-// will print stacktrace
-if (!isProduction) {
-  app.use((err, req, res, next) => {
-    console.log(err.stack);
-
-    res.status(err.status || 500);
-
-    res.json({
-      errors: {
-        message: err.message,
-        error: err
-      }
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.json({
-    errors: {
-      message: err.message,
-      error: {}
-    }
-  });
-});
-
-// finally, let's start our server...
-const server = app.listen(process.env.PORT || 3000, () => {
-  console.log(`Listening on port ${  server.address().port}`);
-});
-
+app.use('*', (_req, res) => res.status(404).json({
+  status: res.statusCode,
+  error: 'Sorry!!, the page you are looking for cannot be found',
+}));
 
 export default app;
